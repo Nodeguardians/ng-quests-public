4de681e155e6e310686ebc998c2ae2ae1847c1a5
bryanwee023 <72504548+bryanwee023@users.noreply.github.com>
2023-04-07 15:34:06 +0000

    Deploy new Quests 1cbbd9

--- campaigns/standalone-quests/using-signatures/contracts/Grandmasters.sol
+++ campaigns/standalone-quests/using-signatures/contracts/Grandmasters.sol
@@ -4,4 +4,13 @@ pragma solidity ^0.8.19;
 import "./interfaces/IGrandmasters.sol";
 
 contract Grandmasters is IGrandmasters {
+
+    mapping(address => bool) public override grandmasters;
+
+    constructor() payable { }
+
+    function acceptInvite(bytes calldata signature) external override { }
+
+    function receiveBlessing(uint256 amount, bytes calldata signature) external override { }
+
 }
