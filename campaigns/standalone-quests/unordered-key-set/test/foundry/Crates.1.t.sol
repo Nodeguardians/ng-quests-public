eddbaeaf60a7232669137b98773ecdda4609cc33
bryanwee023 <72504548+bryanwee023@users.noreply.github.com>
2024-04-11 09:28:10 +0000

    Deploy new Quests e95944

--- campaigns/standalone-quests/unordered-key-set/test/foundry/Crates.1.t.sol
+++ campaigns/standalone-quests/unordered-key-set/test/foundry/Crates.1.t.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.19;
 
 import "./testsuites/TestCrates1.sol";
 
-contract PrivateTest1 is TestCrates1 {
+contract PublicTest1 is TestCrates1 {
 
-    string TEST_DATA_PATH = "test/data/private/crates.json";
+    string TEST_DATA_PATH = "test/data/crates.json";
     constructor() TestCrates1(TEST_DATA_PATH) { }
 
 }
